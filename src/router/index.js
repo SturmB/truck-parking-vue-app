6a009d60dd97c9865083a85046f161fac87b5fbb
Chris McGee <chris@chrismcgee.info>
2023-03-14 17:10:12 -0400

    ✨ Adds registration page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// noinspection NpmUsedModulesInstalled
+
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
-  ]
-})
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("@/views/Auth/RegisterView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
