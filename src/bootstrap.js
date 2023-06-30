d5eeacc57487e2058eaa2bf688098f71ff20ccae
Chris McGee <chris@chrismcgee.info>
2023-06-29 23:12:18 -0400

    🔧 Uses environment variable for the base URL

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,7 +5,7 @@ window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 window.axios.defaults.withCredentials = true;
-window.axios.defaults.baseURL = "http://localhost/api/v1";
+window.axios.defaults.baseURL = process.env.BASE_URL;
 window.axios.interceptors.response.use(
   (response) => response,
   (error) => {
